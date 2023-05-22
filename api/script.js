df56eb4211671b0c6717793309b07143b15c1f48
Hudson de Jesus Ferreira Junior <hudson.junior@MATRIZ.GRUPOADRIANOCOBUCCIO.LOCAL>
2023-05-22 15:19:27 -0300

    Parte 4 - Finalizado

--- api/script.js
+++ api/script.js
@@ -1,12 +1,40 @@
-var consultaCEP = fetch('https://viacep.com.br/ws/37115000/json/')
-    .then(resposta => resposta.json())
-    .then(r => {
-        if (r.erro) {
-            throw Error('Esse cep não existe!')
-        } else
-            console.log(r)
-    })
-    .catch(erro => console.log(erro))
-    .finally(mensagem => console.log('Processamento concluído!'));
+async function buscaEndereco(cep) {
+    try {
+        var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        var consultaCEPConvertida = await consultaCEP.json();
+        if (consultaCEPConvertida.erro) {
+            throw Error('CEP não existente!');
+        }
+        console.log(consultaCEPConvertida);
+        return consultaCEPConvertida;
+    } catch (erro) {
+        console.log(erro);
+    }
+}
 
-console.log(consultaCEP);
+// buscaEndereco();
+
+/* --- Lidando com várias requisições ao mesmo tempo com Promise.all ---
+
+let ceps = ['01001000', '01001001'];
+let conjuntoCeps = ceps.map(valores => buscaEndereco(valores));
+console.log(conjuntoCeps);
+Promise.all(conjuntoCeps).then(respostas => console.log(respostas));
+
+*/
+
+
+//AULA 3
+
+// var consultaCEP = fetch('https://viacep.com.br/ws/37115000/json/')
+//     .then(resposta => resposta.json())
+//     .then(r => {
+//         if (r.erro) {
+//             throw Error('Esse cep não existe!')
+//         } else
+//             console.log(r)
+//     })
+//     .catch(erro => console.log(erro))
+//     .finally(mensagem => console.log('Processamento concluído!'));
+
+// console.log(consultaCEP);
