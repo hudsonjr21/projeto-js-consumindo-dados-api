951ffe43790cd738b2545217815ea0d98c3ca103
Hudson de Jesus Ferreira Junior <hudson.junior@MATRIZ.GRUPOADRIANOCOBUCCIO.LOCAL>
2023-05-22 15:45:07 -0300

    Parte 5 e ultima - Finalizado

--- api/script.js
+++ api/script.js
@@ -1,16 +1,47 @@
 async function buscaEndereco(cep) {
+    var mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML = "";
     try {
         var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
         var consultaCEPConvertida = await consultaCEP.json();
         if (consultaCEPConvertida.erro) {
             throw Error('CEP não existente!');
         }
+        var cidade = document.getElementById('cidade');
+        var logradouro = document.getElementById('endereco');
+        var bairro = document.getElementById('bairro');
+        var estado = document.getElementById('estado');
+
+        cidade.value = consultaCEPConvertida.localidade;
+        logradouro.value = consultaCEPConvertida.logradouro;
+        bairro.value = consultaCEPConvertida.bairro;
+        estado.value = consultaCEPConvertida.uf;
+
         console.log(consultaCEPConvertida);
         return consultaCEPConvertida;
     } catch (erro) {
+        mensagemErro.innerHTML = `<p class="erro__texto">CEP inválido. Tente novamente!</p>`
         console.log(erro);
     }
 }
+
+var cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
+
+//AULA 4
+// async function buscaEndereco(cep) {
+//     try {
+//         var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+//         var consultaCEPConvertida = await consultaCEP.json();
+//         if (consultaCEPConvertida.erro) {
+//             throw Error('CEP não existente!');
+//         }
+//         console.log(consultaCEPConvertida);
+//         return consultaCEPConvertida;
+//     } catch (erro) {
+//         console.log(erro);
+//     }
+// }
 
 // buscaEndereco();
 
